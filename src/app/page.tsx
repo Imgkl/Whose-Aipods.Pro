a1c3fc2419819936f60ee88b73a065f3bbf5bc68
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-02-19 04:16:56 +0530

    Added Theme-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,31 @@ export default function Home() {
     <div>
       <Head>
         <title>Whose-Airpods.Pro</title>
+        <meta name="theme-color" content="#ffffff" />
         <meta name="description" content="Gokul's Airpods Pro" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.backgroundImage}/>
+        <div className={styles.backgroundImage} />
         <div className={styles.centerText}>
           <h1 className={styles.title}>
             Gokul&apos;s
